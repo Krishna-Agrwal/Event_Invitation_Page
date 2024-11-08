cd284f784c48d9f70873c81e80d270f7288353ab
Krishna Agarwal <codekrishna86@gmail.com>
2024-11-08 20:57:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showForm() {
+    document.getElementById("rsvpForm").style.display = "block";
+}
+
+function hideForm() {
+    document.getElementById("rsvpForm").style.display = "none";
+}
